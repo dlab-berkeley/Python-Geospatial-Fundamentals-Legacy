73acd686b6e8e6b7f6fd071a1eff5c7393d0f2c5
Drew Hart <drew.ellison.hart@gmail.com>
2020-11-05 14:45:39 -0800

    Deleted hide_solution_broken() from solution_hider.py.

--- new_sequence/solution_hider.py
+++ new_sequence/solution_hider.py
@@ -35,37 +35,3 @@ def hide_solution():
             <form action="javascript:code_toggle()"><input type="submit" value="CLICK TO TOGGLE SOLUTION"></form>'''
     return HTML(html)
 
-
-def hide_solution_broken(for_next=False):
-    this_cell = """$('div.cell.code_cell.rendered.selected')"""
-    next_cell = this_cell + '.next()'
-
-    toggle_text = 'CLICK TO TOGGLE SOLUTION'  # text shown on toggle link
-    target_cell = this_cell  # target cell to control with toggle
-    js_hide_current = ''  # bit of JS to permanently hide code in current cell (only when toggling next cell)
-
-    if for_next:
-        target_cell = next_cell
-        toggle_text += ' next cell'
-        js_hide_current = this_cell + '.find("div.input").show();'
-
-    js_f_name = 'code_toggle_{}'.format(str(random.randint(1,2**64)))
-
-    html = """
-        <script>
-            function {f_name}() {{
-                {cell_selector}.find('div.input').toggle();
-            }}
-
-            {js_hide_current}
-        </script>
-
-        <a href="javascript:{f_name}()">{toggle_text}</a>
-    """.format(
-        f_name=js_f_name,
-        cell_selector=target_cell,
-        js_hide_current=js_hide_current, 
-        toggle_text=toggle_text
-    )
-    
-    return HTML(html)
